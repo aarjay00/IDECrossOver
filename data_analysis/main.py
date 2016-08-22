05e5f2d56e011de17bda81b42d91aa5275c422cc
Rohit Jain <aarjay@Rohits-MacBook-Pro.local>
2016-08-22 16:13:39 +0400

    basic file open/edit analysis complete

--- data_analysis/main.py
+++ data_analysis/main.py
@@ -1,7 +1,10 @@
 import parse_data
+from file_open_analysis import *
 
 
-command_list=parse_data.parse("eclipse/V1/user2")
-for c in command_list:
-    print c
-print len(command_list)
+command_list,document_change_list=parse_data.parse("eclipse/V1/user2")
+# for c in command_list:
+#     print c
+print len(command_list),len(document_change_list)
+
+file_open_analysis(command_list,document_change_list)
--- data_analysis/parse_data.py
+++ data_analysis/parse_data.py
@@ -15,24 +15,58 @@ def get_all_files(file_path):
 def parse_eclipse(file):
     print file
     with open(file,"r") as fileFD:
-        return xmltodict.parse(fileFD.read())
+        data=fileFD.read()
+        try:
+            return  xmltodict.parse(data)
+        except:
+            print "issues",file
+            try:
+                data+="</Events>"
+                return xmltodict.parse(data)
+            except Exception:
+                print "issues still",file
+                raise Exception
 
-def parse_xml(xml_file,command_num):
+def parse_xml(xml_file,command_num,document_change_num):
     xml_file=xml_file['Events']
     start_timestamp=xml_file['@startTimestamp']
-    commands=xml_file['Command']
+    try:
+        print "eee",xml_file.keys()
+        commands=xml_file['Command']
+        try:
+            document_changes=xml_file['DocumentChange']
+        except:
+            document_changes=[]
+    except:
+        print "aaaa",xml_file.keys()
+        print xml_file["Annotation"]
+        exit()
     command_list=[]
     for command in commands:
         command_dict={}
+        command_dict['log_num']=command['@__id']
         command_dict["command_num"]=command_num
         command_dict['command_type']=command['@_type'] # Command type
         command_dict['timestamp']=float(command['@timestamp'])/1000+float(start_timestamp)/1000 # Command timestamp
-        extra_values=filter(lambda val: val not in ['@timestamp',"@_type"],command.keys())
+        extra_values=filter(lambda val: val not in ['@__id','@timestamp',"@_type"],command.keys())
         for values in extra_values:
             command_dict[values]=command[values]
         command_list.append(command_dict)
         command_num+=1
-    return command_list
+    document_change_list=[]
+    for document_change in document_changes:
+        document_dict={}
+        document_dict['log_num']=document_change['@__id']
+        document_dict['document_change_num']=document_change_num
+        document_dict['action_type']=document_change['@_type']
+        document_dict['offset']=document_change['@offset']
+        document_dict['timestamp']=document_change['@timestamp']
+        extra_values=filter(lambda x : x not in ['@__id','@_type','@offset','@timestamp'],document_change.keys())
+        for values in extra_values:
+            document_dict[values]=document_change[values]
+        document_change_list.append(document_dict)
+        document_change_num += 1
+    return (command_list,document_change_list)
 
 def parse_files(files,log_type):
     file_list=[]
@@ -43,9 +77,13 @@ def parse_files(files,log_type):
             except:
                 continue
     command_list=[]
+    document_change_list=[]
     for file in file_list:
-        command_list.extend(parse_xml(file,len(command_list)))
-    return command_list
+        print files[file_list.index(file)]
+        c,d=parse_xml(file,len(command_list),len(document_change_list))
+        command_list.extend(c)
+        document_change_list.extend(d)
+    return command_list,document_change_list
 # file_path=raw_input()
 
 def parse(file_path):
