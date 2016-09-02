dbe8355e350b0cb475664f01ede178e4454ca7ca
Rohit Jain <aarjay@Rohits-MacBook-Pro.local>
2016-09-02 19:20:05 +0400

    removed migrations files from git

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'workpattern.apps.WorkpatternConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
