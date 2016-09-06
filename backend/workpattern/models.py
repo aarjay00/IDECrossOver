c1e2ff4ed8706c5a9e66ea13962c4a33b65e8405
Rohit Jain <aarjay@ip-192-168-1-133.ec2.internal>
2016-09-06 15:19:37 +0400

    added user dosplay name

--- backend/workpattern/models.py
+++ backend/workpattern/models.py
@@ -9,7 +9,7 @@ class User(models.Model):
 
     user_id = models.AutoField(primary_key=True)
     user_name = models.CharField(max_length=255,unique=True)
-    user_name_display = models.CharField(max_length=255,unique=False)
+    user_name_display = models.CharField(max_length=255,default="display_name",unique=False)
 
 
 class Log(models.Model):
